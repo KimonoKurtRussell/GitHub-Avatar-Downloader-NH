82a36ee34e19f8955fd402d2aaa8ea5c77001ddc
KimonoKurtRussell <n_hardin@hotmail.com>
2018-05-15 07:38:29 +0000

    figured out how to seperate avatart url

--- download_avatars.js
+++ download_avatars.js
@@ -11,16 +11,24 @@ function getRepoContributors(repoOwner, repoName, cb) {
       'User-Agent': 'request',
       'Authorization': 'token ' + password.GITHUB_TOKEN
     }
+  }
 
 
     request(options, function(err, res, body) {
-     cb(err, body);
-   });
- }
+      if (!err) {
+        var info = JSON.parse(body)
+           info.forEach(function (profile) {
+           console.log(profile.avatar_url)
+          })
+          //console.log(info)
+          cb(err, body);
+        }
+    });
+}
 
  getRepoContributors("jquery", "jquery", function(err, result) {
-   console.log("Errors:", err);
-   console.log("Result:", result);
+   //console.log("Errors:", err);
+  // console.log("Result:", result);
  });
 
 
