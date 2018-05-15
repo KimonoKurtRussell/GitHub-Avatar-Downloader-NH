c79bea8325cc53f4177878c9390dda14796ab305
KimonoKurtRussell <n_hardin@hotmail.com>
2018-05-15 08:14:19 +0000

    saving avatars to directory

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,12 @@
 var request = require('request');
-
+var fs = require('fs')
 var password = require('./secrets')
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
-    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
     headers: {
       'User-Agent': 'request',
       'Authorization': 'token ' + password.GITHUB_TOKEN
@@ -16,22 +16,38 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
     request(options, function(err, res, body) {
       if (!err) {
+        cb(err, body);
         var info = JSON.parse(body)
            info.forEach(function (profile) {
-           console.log(profile.avatar_url)
+
           })
-          //console.log(info)
-          cb(err, body);
+
+
         }
     });
 }
 
- getRepoContributors("jquery", "jquery", function(err, result) {
-   //console.log("Errors:", err);
-  // console.log("Result:", result);
- });
+function downloadImageByURL(url, filePath) {
+  request("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
+//      .on('error', function (err) {
+//        throw err;
+//        console.log(error)
+     };
+//      .on('response', function (response) {
+//        console.log('Response Status Code: ', response.statusCode, 'Response Message', response.statusMessage, 'Content Type', response.headers['content-type']);
+//       })
+//      .pipe(fs.createWriteStream("./avatars"))
+//      .on('finish', function () {
+//        console.log('Avatar Download Completed.');
+//      })
+//    console.log(downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg"))
+//  }
 
 
+// getRepoContributors("jquery", "jquery", function(err, result) {
+//    //console.log("Errors:", err);
+//   // console.log("Result:", result);
+ // });
 
 
 
