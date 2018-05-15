481cd5e38f7ddd2bdde684b92a2562d940ae427d
KimonoKurtRussell <n_hardin@hotmail.com>
2018-05-15 05:14:15 +0000

    fixed looping problem with objects

--- download_avatars.js
+++ download_avatars.js
@@ -1,23 +1,46 @@
 var request = require('request');
 
-var password = requires('./secrets')
+var password = require('./secrets')
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
-      'User-Agent': 'request'
+      'User-Agent': 'request',
       'Authorization': 'token ' + password.GITHUB_TOKEN
     }
+
   };
 
   request(options, function(err, res, body) {
     cb(err, body);
   });
 }
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  var data = JSON.parse(result);
+    for (i = 0; i < data.length; i++) {
+
+      if (!err) {
+      downloadImageByURL(data[i].avatar_url, result)
+       console.log(downloadImageByURL())
+      }
+
+    }
+   console.log(data)
+});
+
+function downloadImageByURL(url, filePath) {
+  request.get(url) {
+
+  };
+
+}
+
+
+
 
 
 
