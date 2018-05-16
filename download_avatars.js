b2957d27bb61967adc7aaae35e088842d426402f
KimonoKurtRussell <n_hardin@hotmail.com>
2018-05-16 02:35:15 +0000

    added command line arg.

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,12 @@
 var request = require('request');
 var fs = require('fs')
 var password = require('./secrets')
+var repoOwner = process.argv[2];
+var repoName = process.argv[3];
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-function getRepoContributors(repoOwner, repoName, cb) {
+function getRepoContributors(process1, process2, cb) {
   var options = {
     url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
     headers: {
@@ -18,28 +20,25 @@ function getRepoContributors(repoOwner, repoName, cb) {
       if (!err) {
         cb(err, body);
         var info = JSON.parse(body)
-           info.forEach(function (profile) {
-
+          info.forEach(function (profile) {
           })
-
-
-        }
+      }
     });
 }
 
 function downloadImageByURL(url, filePath) {
-  request("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
+  request(url, filepath)
      .on('error', function (err) {
        throw err;
        console.log(error)
-     })
+      })
      .on('response', function (response) {
        console.log('Response Status Code: ', response.statusCode, 'Response Message', response.statusMessage, 'Content Type', response.headers['content-type']);
       })
-     .pipe(fs.createWriteStream("./avatars/kvirani.png"))
+     .pipe(fs.createWriteStream('./avatars/kvirani.png'))
      .on('finish', function () {
        console.log('Avatar Download Completed.');
-     })
+      })
 
  }
 
@@ -49,7 +48,15 @@ getRepoContributors("jquery", "jquery", function(err, result) {
 
  });
 
-downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
+if (repoOwner == null || repoName == null) {
+  console.log('Repository Name and/or Repository Owner are not valid, try again.')
+} else {
+  getRepoContributors(repoOwner, repoOwner, function (err, res) {
+    console.log('Avatars Downloded')
+       })
+}
+
+
 
 
 
