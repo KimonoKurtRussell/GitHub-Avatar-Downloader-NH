4e51a5bdd04c568e80930951569a30d00c955d93
KimonoKurtRussell <n_hardin@hotmail.com>
2018-05-16 05:43:06 +0000

    submission of final draft, fixed callback working improperly and linted final draft

--- download_avatars.js
+++ download_avatars.js
@@ -6,55 +6,49 @@ var repoName = process.argv[3];
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-function getRepoContributors(process1, process2, cb) {
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+     .on('error', function (err) {
+       console.log('Error occured', url)
+       throw err;
+     })
+     .pipe(fs.createWriteStream(filePath));
+ }
+
+
+if (repoOwner == null || repoName == null) {
+  console.log('Repository Name and/or Repository Owner are not valid, try again.')
+}
+
+
+function getRepoContributors(repoOwner, repoName, avatarRepeat) {
   var options = {
     url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
     headers: {
       'User-Agent': 'request',
       'Authorization': 'token ' + password.GITHUB_TOKEN
     }
-  }
+  };
+  request(options, function(err, res, body) {
+      avatarRepeat(err, body, options);
+  });
+};
 
 
-    request(options, function(err, res, body) {
-      if (!err) {
-        cb(err, body);
-        var info = JSON.parse(body)
-          info.forEach(function (profile) {
-          })
-      }
-    });
-}
-
-function downloadImageByURL(url, filePath) {
-  request(url, filepath)
-     .on('error', function (err) {
-       throw err;
-       console.log(error)
+getRepoContributors(repoOwner, repoName, function(err, result, html) {
+  var info = JSON.parse(result)
+    if (Array.isArray(info) && !error) {
+      info.forEach(function (repoContributor) {
+        downloadImageByURL(repoContributor.avatar_url, './avatars/' + repoContributor.login);
       })
-     .on('response', function (response) {
-       console.log('Response Status Code: ', response.statusCode, 'Response Message', response.statusMessage, 'Content Type', response.headers['content-type']);
-      })
-     .pipe(fs.createWriteStream('./avatars/kvirani.png'))
-     .on('finish', function () {
-       console.log('Avatar Download Completed.');
-      })
-
- }
+    } else {
+        console.log('Cannot access' , html.url)
+         return
+       }
+        console.log(info.length + 'Avatars Downloded')
+});
 
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-   console.log("Errors:", err);
-
- });
-
-if (repoOwner == null || repoName == null) {
-  console.log('Repository Name and/or Repository Owner are not valid, try again.')
-} else {
-  getRepoContributors(repoOwner, repoOwner, function (err, res) {
-    console.log('Avatars Downloded')
-       })
-}
 
 
 
